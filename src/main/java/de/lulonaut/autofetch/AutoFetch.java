d9bdbd0ae6e98dd9c094e911b94b18eca67d498e
Lulonaut <lulonaut@tutanota.de>
2022-11-22 20:28:54 +0100

    switch to common fork pool

--- src/main/java/de/lulonaut/autofetch/AutoFetch.java
+++ src/main/java/de/lulonaut/autofetch/AutoFetch.java
@@ -1,6 +1,5 @@
 package de.lulonaut.autofetch;
 
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import de.lulonaut.autofetch.settings.AppSettingsState;
@@ -10,16 +9,14 @@ import git4idea.repo.GitRepository;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 public class AutoFetch implements StartupActivity, StartupActivity.DumbAware {
-    private ExecutorService executorService = Executors.newFixedThreadPool(100);
 
     @Override
     public void runActivity(@NotNull Project project) {
-        executorService.submit(() -> {
+        ForkJoinPool.commonPool().submit(() -> {
             while (true) {
                 if (AppSettingsState.getInstance().getEnabledStatus()) {
                     //stop if the project is no longer open
@@ -36,8 +33,6 @@ public class AutoFetch implements StartupActivity, StartupActivity.DumbAware {
                             gitFetchSupport.fetchAllRemotes(repositories);
                         }
                     }
-
-
                 }
 
                 //wait out the delay
