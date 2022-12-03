a4c1feb182bffee02544bde38c268fae76e59282
Lulonaut <lulonaut@tutanota.de>
2022-12-03 13:42:56 +0100

    gracefully exit when project is closed

--- src/main/java/de/lulonaut/autofetch/AutoFetch.java
+++ src/main/java/de/lulonaut/autofetch/AutoFetch.java
@@ -1,6 +1,5 @@
 package de.lulonaut.autofetch;
 
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.util.concurrency.AppExecutorUtil;
@@ -11,21 +10,21 @@ import git4idea.repo.GitRepository;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class AutoFetch implements StartupActivity, StartupActivity.DumbAware {
-    private static final Logger LOG = Logger.getInstance(AutoFetch.class);
 
     @Override
     public void runActivity(@NotNull Project project) {
-        AppExecutorUtil.getAppScheduledExecutorService().scheduleWithFixedDelay(() -> {
+        final CompletableFuture<ScheduledFuture<?>> gitFetchFuture = new CompletableFuture<>();
+        final ScheduledFuture<?> task = AppExecutorUtil.getAppScheduledExecutorService().scheduleWithFixedDelay(() -> {
             if (AppSettingsState.getInstance().getEnabledStatus()) {
                 //stop if the project is no longer open
-                LOG.warn("run");
                 if (project.isDisposed()) {
-                    LOG.warn("exit");
-                    throw new RuntimeException();
+                    gitFetchFuture.cancel(true);
                 }
 
                 //get all connected git repos and sort out repos with no remotes
@@ -38,6 +37,7 @@ public class AutoFetch implements StartupActivity, StartupActivity.DumbAware {
                     }
                 }
             }
-        }, 0, AppSettingsState.getInstance().getAutoFetchDelay(), TimeUnit.MINUTES);
+        }, 0, 10, TimeUnit.SECONDS);
+        gitFetchFuture.complete(task);
     }
 }
