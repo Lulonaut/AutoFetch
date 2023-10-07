b89af2e925699b010624b0d674e45e2c1f94ca9e
Lulonaut <lulonaut@lulonaut.tech>
2023-10-07 19:24:21 +0200

    Remove untilBuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "de.lulonaut"
-version = "1.3"
+version = "1.5"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("213")
-        untilBuild.set("223.*")
+        untilBuild.set("")
     }
 
     signPlugin {
--- src/main/java/de/lulonaut/autofetch/AutoFetch.java
+++ src/main/java/de/lulonaut/autofetch/AutoFetch.java
@@ -1,6 +1,5 @@
 package de.lulonaut.autofetch;
 
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.util.concurrency.AppExecutorUtil;
